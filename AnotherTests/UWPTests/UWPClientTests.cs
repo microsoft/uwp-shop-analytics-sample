87575692b6a2cb06fe6877b0caefd011b7efc231
Rido Min <ridomin@live.com>
2016-08-01 00:18:12 -0700

    add client and UWP Tests

---
+++ AnotherTests/UWPTests/UWPClientTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using ShopAnalyticsPCL;
+using System.Threading.Tasks;
+
+namespace UWPTests
+{
+    [TestClass]
+    public class UWPClientTests
+    {
+        Client client = new Client();
+
+        [TestMethod]
+        public async Task CreateEvent()
+        {
+            await client.CreateEvent(true);
+            await client.CreateEvent(false);
+        }
+
+
+        [TestMethod]
+        public async Task ReadAll()
+        {
+            var events = await client.ReadAllEvents();
+            Assert.IsNotNull(events);
+            foreach (var te in events)
+            {
+                System.Diagnostics.Debug.WriteLine($"type {te.EventType} on {te.EventTime.ToString()}");
+            }
+        }
+    }
+}
---
+++ ShopAnalyticsPCL/Client.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using ShopAnalyticsPCL.Models;
+using ShopAnalyticsPCL.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAnalyticsPCL
+{
+    public class Client
+    {
+        HttpClient http = new HttpClient() { BaseAddress = new Uri(Keys.AzureWebAppUri)};
+
+        public async Task CreateEvent(bool eventType)
+        {
+            var newEvent = new TriggeredEvent
+            {
+                EventType = eventType,
+                EventTime = DateTime.Now
+            };
+
+            HttpContent contentPost = new StringContent(JsonConvert.SerializeObject(newEvent), System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage message = await http.PostAsync("api/event", contentPost);
+        }
+
+        public async Task<IList<TriggeredEvent>> ReadAllEvents()
+        {
+            var response = await http.GetAsync("api/event");
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IList<TriggeredEvent>>(content);
+        }
+    }
+}
