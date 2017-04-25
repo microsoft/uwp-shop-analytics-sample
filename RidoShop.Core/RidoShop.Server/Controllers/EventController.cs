fbe5cb0b5fe7922c6982be96689c7e96ca8236fd
ridomin <ridomin@live.com>
2017-04-24 18:13:36 -0700

    adding ASP.NET Core

--- RidoShop.Core/RidoShop.Server/Controllers/EventController.cs
+++ RidoShop.Core/RidoShop.Server/Controllers/EventController.cs
@@ -4,31 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Http;
-using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Client;
+using Microsoft.AspNetCore.Mvc;
+using RidoShop.Server;
+using ShopEvents.Models;
 using Microsoft.Azure.NotificationHubs;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using ShopAnalyticsPCL.Models;
-using ShopAnalyticsPCL.Resources;
-using EventApi.DocDb;
 
 namespace EventApi.Controllers
 {
-    public class EventController : ApiController
+    public class EventController : Controller
     {
         // URI for docDB service
-        private readonly string endpointUri = Keys.DocDbUri;
+        private readonly string endpointUri = "";// Keys.DocDbUri;
 
         // Defines the exact pointer to the document used for this program
-        private readonly string databaseName = Keys.DocDbName;
-        private readonly string collectionName = Keys.DocDbCollectionName;
-        private readonly string documentName = Keys.DocDbDocName;
+        private readonly string databaseName = "";//  Keys.DocDbName;
+        private readonly string collectionName = "";// Keys.DocDbCollectionName;
+        private readonly string documentName = "";// Keys.DocDbDocName;
 
-        private readonly string docDbKey = Keys.DocDbKey;
+        private readonly string docDbKey = "";//Keys.DocDbKey;
 
-        private readonly TriggeredEventRepository repository = new TriggeredEventRepository(new Uri(Keys.DocDbUri), Keys.DocDbKey);
+        private readonly TriggeredEventRepository repository = new TriggeredEventRepository(
+            new Uri(""), 
+            "");
 
         // GET: api/Event/5
         /// <summary>
@@ -54,15 +51,17 @@ namespace EventApi.Controllers
             
             var pushNotificationResponse = await TriggerPushNotification(newEvent.EventType);
 
-            return base.Request.CreateResponse(HttpStatusCode.OK, newEvent);
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.OK);
+            return res;
+            //return Request.CreateResponse(HttpStatusCode.OK, newEvent);
         }
 
         private async Task<NotificationOutcome> TriggerPushNotification(bool eventType)
         {
             string windowsToastPayload;
             // Get the Notification Hubs credentials for the Mobile App.
-            string notificationHubName = Keys.NhName;
-            string notificationHubConnection = Keys.NhFullConnection;
+            string notificationHubName = "";//Keys.NhName;
+            string notificationHubConnection = "";//Keys.NhFullConnection;
 
             // Create the notification hub client.
             var hub = NotificationHubClient
