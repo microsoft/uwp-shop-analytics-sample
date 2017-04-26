9171d5a171046bcf3c702b5f97ff8e91e394762f
ridomin <ridomin@live.com>
2017-04-25 18:11:11 -0700

    remove client

--- RidoShop.Core/RidoShop.Sensors/Properties/AssemblyInfo.cs
+++ RidoShop.Core/RidoShop.Sensors/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RidoShop.Client")]
+[assembly: AssemblyTitle("RidoShop.Sensors")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RidoShop.Client")]
-[assembly: AssemblyCopyright("Copyright ©  1976")]
+[assembly: AssemblyProduct("RidoShop.Sensors")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
